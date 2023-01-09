fd69c538bb1e0cee8be682ecc30e438cdb725ed8
Luis Franco <lfranco21@users.noreply.github.com>
2023-01-09 11:13:03 -0500

    first commit

---
+++ conjuntos.js
@@ -0,0 +1,10 @@
+const miFamilia = ["Luis Franco", "Emma Rojas", "Enmilys Franco", "Eyla Franco", "Alanis Franco", "Luis Ernesto Franco"];
+
+const miSet = new Set(miFamilia);
+console.log(miSet);
+
+miSet.add("Luis Ernesto Franco");
+console.log(miSet);
+
+miSet.add("Javascript");
+console.log(miSet);
